b19ef495103aa06a105402fa3cf849019a0a3fd1
toxnroot <mohammedking.655@gmail.com>
2024-12-31 04:48:18 +0200

    edit css class

--- src/app/components/ButtonDownload/ButtonDownload.jsx
+++ src/app/components/ButtonDownload/ButtonDownload.jsx
@@ -1,9 +1,10 @@
 import './ButtonDownload.css'
 
 const ButtonDownload = ({download,text,color}) => {
+  
   return (
       <button className="Btn-Container" onClick={download}>
-        <span className="text">{text}</span>
+        <span className="text-down">{text}</span>
         <span className={`icon-Container ${color}`}>
           <svg width={16} height={19} viewBox="0 0 16 19" fill="nones" xmlns="http://www.w3.org/2000/svg">
             <circle cx="1.61321" cy="1.61321" r="1.5" fill="black" />
